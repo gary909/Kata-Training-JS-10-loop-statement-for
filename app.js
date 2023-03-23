10a3ffbfa0fac844a54f9cad840510d1b1dc17c1
gary909 <garywhite61@hotmail.com>
2023-03-23 12:43:53 +0100

    code working

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 function pickIt(arr){
   var odd=[],even=[];
-  //coding here
-  for(let i =0; i < arr.length;i++){
-    if(i % 0 == 0){
-      odd.push[i];
+  for(let i = 0; i < arr.length;i++){
+    if(arr[i] % 2 == 0){
+      even.push(arr[i]);
     } else {
-      even.push[i];
+      odd.push(arr[i]);
     }
   }
-  
-  
   return [odd,even];
 }
 
